dfdd75b7df6ef36f40dd275df2410734df75b88a
Catalina Castro <catalina.cst@utp.edu.co>
2017-09-17 22:24:57 -0500

    Temperatura

---
+++ Workshop/temperature.js
@@ -0,0 +1,12 @@
+var five = require("johnny-five");
+
+five.Board().on("ready", function() {
+  var temperature = new five.Thermometer({
+    controller: "TMP36",
+    pin: "A0"
+  });
+
+  temperature.on("change", function() {
+    console.log(this.celsius + "°C", this.fahrenheit + "°F");
+  });
+});
