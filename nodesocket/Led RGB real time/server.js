ca1d4c33c1299d1621c4ff806865ef877260860d
Catalina Castro <catalina.cst@utp.edu.co>
2017-09-19 00:21:42 -0500

    Led RGB + web socket

---
+++ nodesocket/Led RGB real time/server.js
@@ -0,0 +1,51 @@
+var http = require('http');
+var express = require('express');
+var five = require('johnny-five');
+var app = express();
+var server = http.createServer(app);
+var board = new five.Board();
+
+app.use(express.static(__dirname + '/web'));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/web/index.html');
+});
+
+server.listen(8080);
+
+var io = require('socket.io').listen(server);
+
+board.on("ready", function() {
+
+	io.on('connection', function(socket){
+		console.log("Persona conectada");
+	  // Create a new `ledRGB` hardware instance.
+		var anode = new five.Led.RGB({
+	    pins: {
+	      red: 6,
+	      green: 5,
+	      blue: 3
+	    },
+	    isAnode: true
+	  });
+
+		socket.on('darColor', function(colorHexa, colorNombre){
+			anode.color(colorHexa);
+			io.sockets.emit('mostrarColor', colorNombre);
+		});
+
+		var index = 0;
+		var rainbow = ["FF0000", "FF7F00", "FFFF00", "00FF00", "0000FF", "4B0082", "8F00FF"];
+
+		socket.on('strobe', function(){
+			board.loop(500, function() {
+		    anode.color(rainbow[index++]);
+		    if (index === rainbow.length) {
+		      index = 0;
+		    }
+		  });
+			io.sockets.emit('mostrarColor', "PARTY!!");
+		});
+
+	});
+});
---
+++ nodesocket/Led RGB real time/web/main.js
@@ -0,0 +1,44 @@
+var socket = io.connect('http://localhost:8080', { 'forceNew': true });
+
+
+function render (colorNombre) {
+  document.getElementById('color').innerHTML = colorNombre;
+}
+
+function colorRojo(e) {
+  var colorNombre = document.getElementById('Rojo').id;
+  var colorHexa = document.getElementById('Rojo').value;
+  socket.emit('darColor', colorHexa, colorNombre);
+  return false;
+}
+
+function colorAmarillo(e) {
+  var colorNombre = document.getElementById('Amarillo').id;
+  var colorHexa = document.getElementById('Amarillo').value;
+  socket.emit('darColor', colorHexa, colorNombre);
+  return false;
+}
+
+function colorVerde(e) {
+  var colorNombre = document.getElementById('Verde').id;
+  var colorHexa = document.getElementById('Verde').value;
+  socket.emit('darColor', colorHexa, colorNombre);
+  return false;
+}
+
+function colorAzul(e) {
+  var colorNombre = document.getElementById('Azul').id;
+  var colorHexa = document.getElementById('Azul').value;
+  socket.emit('darColor', colorHexa, colorNombre);
+  return false;
+}
+
+function funcionStrobe(e) {
+  socket.emit('strobe');
+  return false;
+}
+
+socket.on('mostrarColor', function(colorNombre){
+  console.log("8F00FF");
+  render(colorNombre);
+});
